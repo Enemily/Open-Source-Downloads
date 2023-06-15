7a173844eab9f8b406fcad294edede0de87f78c8
Enemily <92351379+Enemily@users.noreply.github.com>
2023-06-14 21:28:30 -0700

    Update get_size.py

--- dataset_curation/get_size.py
+++ dataset_curation/get_size.py
@@ -9,18 +9,36 @@ import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
 import re
+import git
+from datetime import date, time
+import requests
+from prometheus_client import start_http_server, Summary, Counter
+from pydriller.repository import Repository  
+from pydriller.metrics.process.commits_count import CommitsCount
+import pydriller # Pydriller is the library we use to go through the commits on disc 
+from git import Repo, Commit
+import csv
+import os
+import pandas as pd
+import numpy as np
+from git import Repo
+from builtins import any as b_any
+import stscraper as scraper
+import warnings
+warnings.filterwarnings("ignore")
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from time import strptime
+import re
 
-d = pd.read_csv("/data3/emilyngu/final_subset_NA.csv")
+d = pd.read_csv("final_subset_peaked_early.csv")
 df = pd.DataFrame(d)
-df2 = pd.read_csv("/data3/emilyngu/final_subset_NA_unique_slugs.csv")
+df['size'] = None
+df2 = pd.DataFrame(df['slug'].unique(), columns=['slug'])
 
 for i in range(0,len(df2)):
     s = orig = df2['slug'][i]
     ind = (df['slug'] == orig)
-    if re.search(r'    .*', df['size'][ind].head(1).to_string()) is not None and re.search(r'0\.0', df['size'][ind].head(1).to_string()) is None:
-        print(str(i)+", "+orig)
-        continue
-
 
     try:
         if s.find('/tree/') != -1:
@@ -72,120 +90,53 @@ for i in range(0,len(df2)):
     # count = 0
     flag = False
 
-    ########## if path doesn't exist, clone it to tardigrade databse /data3/emilyngu
     if os.path.exists(a) == False:
-        try:
-            Repo.clone_from("https://github.com/"+st, a)
-        except:
-            print("Error with cloning repo, "+str(i)+", "+orig)
-            continue 
+        a = "/data2/christian/npm/repos/"+st
+        if os.path.exists(a) == False:
+            a = "/data2/christian/npm/repos2/"+st
+            if os.path.exists(a) == False:
+                try:
+                    a = "/data3/emilyngu/" + st
+                    Repo.clone_from("https://github.com/"+st, a)
+                except:
+                    print("Error with cloning repo, "+str(i)+", "+a)
+                    continue 
+                if os.path.exists(a) == False:
+                    print("Error with cloning repo, "+str(i)+", "+a)
+                    continue 
     
     os.chdir(a)
      
-    start_date = re.search(r'\w{4}-\w{2}', df['yearMonth'][ind].head(1).to_string()).group()
     end_date = re.search(r'\w{4}-\w{2}', df['yearMonth'][ind].tail(1).to_string()).group()
-    start_year = int(start_date[0:4])
-    end_year = int(end_date[0:4]) 
-    start_month = int(start_date[5:7]) 
+    end_year = int(end_date[0:4])  
     last_month = int(end_date[5:7])
-            
 
-    # first year
-    for month in range(start_month, 13):
-        if month<10:
-            date = start_date[0:4]+"-0"+str(month)
+
+    if last_month<10:
+        date = str(end_year)+"-0"+str(last_month)
+    else:
+        date = str(end_year)+"-"+str(last_month)
+    # try:
+    try:
+        commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'master'], stderr=subprocess.DEVNULL).decode().strip()
+    except:
+        try:
+            commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'main'], stderr=subprocess.DEVNULL).decode().strip()
+        except:
+            try:
+                commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'HEAD'], stderr=subprocess.DEVNULL).decode().strip()
+            except:
+                continue
+    try:
+        if commit_hash != '':
+            subprocess.run(['git', 'checkout', commit_hash], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, check=True)
+            command = ['du', '-sb', '--exclude=.git', a]
+            output = subprocess.check_output(command).decode().strip()
+            size_in_bytes = int(output.split()[0])
         else:
-            date = start_date[0:4]+"-"+str(month)
-            # try:
-        try:
-            commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'master'], stderr=subprocess.DEVNULL).decode().strip()
-        except:
-            try:
-                commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'main'], stderr=subprocess.DEVNULL).decode().strip()
-            except:
-                try:
-                    commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'HEAD'], stderr=subprocess.DEVNULL).decode().strip()
-                except:
-                    print("Error with getting size for "+orig+", "+str(i)+", on date: "+date)
-                    continue
-        try:
-            if commit_hash != '':
-                subprocess.run(['git', 'checkout', commit_hash], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, check=True)
-                command = ['du', '-sb', '--exclude=.git', a]
-                output = subprocess.check_output(command).decode().strip()
-                size_in_bytes = int(output.split()[0])
-            else:
-                size_in_bytes = 0
-            pos = (df['slug'] == orig) & (df['yearMonth'] == date)
-            df.loc[ pos , 'size'] = size_in_bytes/1000 
-        except:
-            print("Error with getting size for "+orig+", "+str(i)+", on date: "+date)  
+            size_in_bytes = 0
+        df.loc[ ind , 'size'] = size_in_bytes/1000 
+    except:
+        print("Error with getting size for "+orig+", "+str(i)+", on date: "+date)
 
-
-    # middle years
-    for year in range (start_year+1, end_year):
-        for month in range(1, 13):
-            if month<10:
-                date = str(year)+"-0"+str(month)
-            else:
-                date = str(year)+"-"+str(month)
-            # try:
-            try:
-                commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'master'], stderr=subprocess.DEVNULL).decode().strip()
-            except:
-                try:
-                    commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'main'], stderr=subprocess.DEVNULL).decode().strip()
-                except:
-                    try:
-                        commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'HEAD'], stderr=subprocess.DEVNULL).decode().strip()
-                    except:
-                        print("Error with getting size for "+orig+", "+str(i)+", on date: "+date)
-                        continue
-            try:
-                if commit_hash != '':
-                    subprocess.run(['git', 'checkout', commit_hash], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, check=True)
-                    command = ['du', '-sb', '--exclude=.git', a]
-                    output = subprocess.check_output(command).decode().strip()
-                    size_in_bytes = int(output.split()[0])
-                else:
-                    size_in_bytes = 0
-                pos = (df['slug'] == orig) & (df['yearMonth'] == date)
-                df.loc[ pos , 'size'] = size_in_bytes/1000 
-            except:
-                print("Error with getting size for "+orig+", "+str(i)+", on date: "+date)
-
-    # last year
-    for month in range(1, last_month+1):
-        if month<10:
-            date = str(end_year)+"-0"+str(month)
-        else:
-            date = str(end_year)+"-"+str(month)
-        # try:
-        try:
-            commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'master'], stderr=subprocess.DEVNULL).decode().strip()
-        except:
-            try:
-                commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'main'], stderr=subprocess.DEVNULL).decode().strip()
-            except:
-                try:
-                    commit_hash = subprocess.check_output(['git', 'rev-list', '-n', '1', f'--before="{date}"', 'HEAD'], stderr=subprocess.DEVNULL).decode().strip()
-                except:
-                    print("Error with getting size for "+orig+", "+str(i)+", on date: "+date)
-                    continue
-        try:
-            if commit_hash != '':
-                subprocess.run(['git', 'checkout', commit_hash], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, check=True)
-                command = ['du', '-sb', '--exclude=.git', a]
-                output = subprocess.check_output(command).decode().strip()
-                size_in_bytes = int(output.split()[0])
-            else:
-                size_in_bytes = 0
-            pos = (df['slug'] == orig) & (df['yearMonth'] == date)
-            df.loc[ pos , 'size'] = size_in_bytes/1000 
-        except:
-            print("Error with getting size for "+orig+", "+str(i)+", on date: "+date)    
-    if i%10 == 0:
-        print(orig + " "+str(i))
-        df.to_csv("/data3/emilyngu/final_subset_NA_size.csv")
-df.to_csv("/data3/emilyngu/final_subset_NA_size.csv")
-
+df.to_csv("final_subset_peaked_early.csv")
